605396311e72c05bab0e56056adf04121c1ef7dc
Victoria Tkachenko <tkachenko@insysbio.ru>
2018-12-06 13:26:37 +0300

    add ;

--- bin/slv-parse.js
+++ bin/slv-parse.js
@@ -1,20 +1,20 @@
 #!/usr/bin/env node
-const commander = require('commander')
-const fs = require('fs')
-const path = require('path')
-const slvParse = require('../src').slvParse
+const commander = require('commander');
+const fs = require('fs');
+const path = require('path');
+const slvParse = require('../src').slvParse;
 
 commander
   .description('parse slv file')
   .usage("[inputPath]")
   .option('-o, --output <path>', 'output file after parse')
   .action((input, cmd) => {
-    let output = cmd.output
+    let output = cmd.output;
     fs.readFile(path.resolve(__dirname, '../', input), 'utf8', (err, contents) => {
-      if (err) throw err
-      let result = slvParse.parse(contents)
+      if (err) throw err;
+      let result = slvParse.parse(contents);
 
-      fs.writeFile(path.resolve(__dirname, '../', output), JSON.stringify(result, null, 2))
+      fs.writeFile(path.resolve(__dirname, '../', output), JSON.stringify(result, null, 2));
     })
   })
-  .parse(process.argv)
+  .parse(process.argv);
