27fc2a91ccf2bc5c59130c3be2ad2cac17dabec9
Evgeny Metelkin <metelkin@insysbio.com>
2020-01-18 13:43:29 +0300

    add case with expression in IV

--- test/slv.js
+++ test/slv.js
@@ -18,22 +18,25 @@ describe('SLV exports', () => {
         , hetajsActual // conversion to Heta JSON
         , heta; // Heta to compare
 
-      // read files
+      // source file
       slv = fs.readFileSync(
         path.resolve(__dirname, x.sourcePath),
         'utf8'
       );
+      // slvjs file
       let slvjsStr = fs.readFileSync(
         path.resolve(__dirname, x.slvjsPath),
         'utf8'
       );
       slvjs = JSON.parse(slvjsStr);
-      let hetajsStr = fs.readFileSync(
+      // hetajs file
+      let hetajsStr = x.hetajsPath && fs.readFileSync(
         path.resolve(__dirname, x.hetajsPath),
         'utf8'
       );
-      hetajs = JSON.parse(hetajsStr);
-      heta = fs.readFileSync(
+      hetajs = hetajsStr && JSON.parse(hetajsStr);
+      // heta file
+      heta = x.hetaPath && fs.readFileSync(
         path.resolve(__dirname, x.hetaPath),
         'utf8'
       );
@@ -43,13 +46,13 @@ describe('SLV exports', () => {
         expect(slvjsActual).to.be.deep.equal(slvjs);
       });
 
-      it('convert to Heta JSON module', () => {
+      hetajs && it('convert to Heta JSON module', () => {
         let hetajsActualString = slvjs2heta(slvjsActual, true);
         hetajsActual = JSON.parse(hetajsActualString);
         expect(hetajsActual).to.be.deep.equal(hetajs);
       });
       
-      it('convert to Heta module', () => {
+      heta && it('convert to Heta module', () => {
         let hetaActual = slvjs2heta(slvjsActual, false, true);
         expect(hetaActual).to.be.equal(heta);
       });
